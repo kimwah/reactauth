f7194b88143e3102f7be6c88b13e06a0eeefa9ad
kimwah <kimwahwong@gmail.com>
2017-04-16 18:53:08 +0800

    adding firebase setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React , { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyB-I14DBTYmlx9rq_wYXE6NNcw_OiKlr4o",
+            authDomain: "authentication-b1ae0.firebaseapp.com",
+            databaseURL: "https://authentication-b1ae0.firebaseio.com",
+            projectId: "authentication-b1ae0",
+            storageBucket: "authentication-b1ae0.appspot.com",
+            messagingSenderId: "1079381687357"
+        });
+    }
     render() {
         return (
         <View>
-            <Text style={{fontSize: 18, color: 'blue'}}>Wong Xin Yang China Yoyo King</Text>
+            <Header headerText="Authentication" />
+            <Text style={{fontSize: 18, color: 'blue'}}>Auth App</Text>
         </View> 
         );   
     }
