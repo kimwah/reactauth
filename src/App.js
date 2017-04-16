13bcdaa7cdb7b54d75ba9cfa099bda6970567e2a
kimwah <kimwahwong@gmail.com>
2017-04-16 17:48:35 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React , { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+    render() {
+        return (
+        <View>
+            <Text style={{fontSize: 18, color: 'blue'}}>Wong Xin Yang China Yoyo King</Text>
+        </View> 
+        );   
+    }
+}
+
+export default App;
